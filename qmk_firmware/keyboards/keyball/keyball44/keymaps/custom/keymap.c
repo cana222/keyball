36643f8d3b46d985b825258549968ff8d24686f9
cana222 <chimney.sweeper06@gmail.com>
2026-03-06 09:55:48 +0900

    修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/custom/keymap.c
@@ -56,6 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
+#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
+    keyball_handle_auto_mouse_layer_change(state);
+#endif
     return state;
 }
 
@@ -68,8 +71,4 @@ void oledkit_render_info_user(void) {
     keyball_oled_render_ballinfo();
     keyball_oled_render_layerinfo();
 }
-#endif
-
-#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
-    keyball_handle_auto_mouse_layer_change(state);
 #endif
